cb2788629d3e66f237cb505e6a16748d465fcb9c
Mathias Johansson <mathias9807@gmail.com>
2023-01-28 15:57:38 +0100

    Adjust fretboard graph

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fretvis/music"
+	"math"
 	"os"
 
 	"github.com/therecipe/qt/core"
@@ -52,8 +53,11 @@ func main() {
 type Fretboard struct {
 	widgets.QWidget
 	margin    int
+	nutM      int
 	nStrings  int
+	nFrets    int
 	strMargin int
+	fretBLen  int
 }
 
 func NewFretboard() *Fretboard {
@@ -61,9 +65,12 @@ func NewFretboard() *Fretboard {
 		QWidget: *widgets.NewQWidget(nil, 0),
 	}
 	f.margin = 10
+	f.nutM = 8
 	f.nStrings = 6
-	f.strMargin = 20
-	contentW := 360
+	f.nFrets = 24
+	f.strMargin = 25
+	f.fretBLen = 600
+	contentW := f.fretBLen + f.nutM
 	contentH := (f.nStrings - 1) * f.strMargin
 	f.SetFixedSize2(contentW+2*f.margin, contentH+2*f.margin)
 	f.ConnectPaintEvent(f.draw)
@@ -78,9 +85,16 @@ func (f *Fretboard) draw(paintEvent *gui.QPaintEvent) {
 
 	w, h := f.Width(), f.Height()
 	m := f.margin
-	nutM := 8
+	nutM := f.nutM
 	nutStrokeW := 4
 	strStrokeW := 2
+
+	// Calculate the distance up the fretboard a given fret is
+	strDistOfFret := func(fret float64, width int) float64 {
+		maxFret := float64(f.nFrets)
+		strLen := float64(width) / (1.0 - math.Pow(2.0, -maxFret/12.0))
+		return strLen - strLen/math.Pow(2.0, (fret/12.0))
+	}
 
 	// Draw nut
 	painter.Pen().SetWidth(nutStrokeW)
@@ -101,6 +115,27 @@ func (f *Fretboard) draw(paintEvent *gui.QPaintEvent) {
 			lastLine = nutStrokeW/2 - strStrokeW/2
 		}
 		painter.DrawLine3(m+nutM, m+i*f.strMargin-firstLine+lastLine, w-m+1, m+i*f.strMargin-firstLine+lastLine)
+	}
+
+	// Draw frets
+	for i := 0; i <= f.nFrets; i++ {
+		fretDist := int(strDistOfFret(float64(i), f.fretBLen))
+		painter.DrawLine3(m+nutM+fretDist, m, m+nutM+fretDist, h-m)
+	}
+
+	// Draw fret dots
+	dotSize := 4
+	painter.SetRenderHint(gui.QPainter__Antialiasing, true)
+	painter.Brush().SetStyle(core.Qt__SolidPattern)
+	fretDots := []float32{3, 5, 7, 9, 12, 15, 17, 19, 21, 24}
+	for _, fretDot := range fretDots {
+		fretDotOffs := (strDistOfFret(float64(fretDot-1.0), f.fretBLen-1) + strDistOfFret(float64(fretDot), f.fretBLen-1)) / 2.0
+		if int(fretDot)%12 == 0 {
+			painter.DrawEllipse3(m+nutM+int(fretDotOffs)-dotSize/2+1, m+int(1.5*float32(f.strMargin))-dotSize/2, dotSize, dotSize)
+			painter.DrawEllipse3(m+nutM+int(fretDotOffs)-dotSize/2+1, m+int(3.5*float32(f.strMargin))-dotSize/2, dotSize, dotSize)
+		} else {
+			painter.DrawEllipse3(m+nutM+int(fretDotOffs)-dotSize/2+1, m+int(2.5*float32(f.strMargin))-dotSize/2, dotSize, dotSize)
+		}
 	}
 
 	painter.End()
